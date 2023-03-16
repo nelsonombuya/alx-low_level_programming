bfb0999690aa6666cfce0de377e37f39840c7d59
nelsonombuya <nelson.ombuya@zohomail.com>
2023-03-16 12:27:55 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 fprintf(stderr, "%s", "and that piece of art is useful\" ");
-fprintf(stderr, "%s", "- Dora Korpar, 2015-10-19\n");;
+fprintf(stderr, "%s", "- Dora Korpar, 2015-10-19\n");
 return (1);
 }
