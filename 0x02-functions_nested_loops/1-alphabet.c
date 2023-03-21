14915e8eb2164d74bd04390575f6856068c73f31
nelsonombuya <nelson.ombuya@zohomail.com>
2023-03-21 11:37:02 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints "_putchar" followed by a new line
+ * print_alphabet - prints the alphabet in lowercase
  * Return: void
  */
 void print_alphabet(void)
