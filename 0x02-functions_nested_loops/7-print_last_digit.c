297534524d62a1b3e9df1bdb4848f93f925862ea
nelsonombuya <nelson.ombuya@zohomail.com>
2023-03-21 12:00:17 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ * @num: integer to be processed
+ * Return: last digit of @num
+ */
+int print_last_digit(int num)
+{
+	return (_abs(result));
+}
