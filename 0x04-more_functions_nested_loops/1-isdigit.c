3f567246981c5e484f23dbdfb257f43ba0e88c76
nelsonombuya <nelson.ombuya@zohomail.com>
2023-03-23 14:03:41 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,9 +2,9 @@
 
 /**
  * _isdigit - checks if a character is a digit
- * 
+ *
  * @c: integer parameter
- * 
+ *
  * Return: 1 if true, 0 if false
  */
 
@@ -12,7 +12,6 @@ int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
-
 	else
 		return (0);
 }
