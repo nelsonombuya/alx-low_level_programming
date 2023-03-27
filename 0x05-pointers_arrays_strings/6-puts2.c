247843e2c6fca4195a3f7b65d5842aba07a3be9c
nelsonombuya <nelson.ombuya@zohomail.com>
2023-03-27 13:14:07 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of string
+ * @str: string string
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(str[a]);
+		}
+		a++;
+	}
+	_putchar('\n');
+}
