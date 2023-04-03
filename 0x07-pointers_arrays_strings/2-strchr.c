b3fd5789ceef7ee2063f8c9cdd655bff5bed6d0b
nelsonombuya <nelson.ombuya@zohomail.com>
2023-04-03 11:39:17 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,8 +14,10 @@ char *_strchr(char *s, char c)
 {
 	int index;
 
-	for (index = 0; s[index] != '\0'; index++)
+	for (index = 0; s[index] >= '\0'; index++)
+	{
 		if (s[index] == c)
 			return (s + index);
+	}
 	return ('\0');
 }
