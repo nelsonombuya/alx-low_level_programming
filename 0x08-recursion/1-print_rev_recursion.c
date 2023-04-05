9161389cee00ed6deee2cfcf2e75798038436d27
nelsonombuya <nelson.ombuya@zohomail.com>
2023-04-05 13:37:15 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion
+ * Description: prints a string in reverse
+ *
+ * @s: character of a string
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
