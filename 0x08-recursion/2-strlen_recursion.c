855214be8586bc07aa8ea2f904842562eb3e0de0
nelsonombuya <nelson.ombuya@zohomail.com>
2023-04-05 13:45:48 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * _strlen_recursion
- * Description: returns the length of a string.
+ * _strlen_recursion - returns the length of a string.
  *
  * @s: pointer to string
  * Return: length of string
