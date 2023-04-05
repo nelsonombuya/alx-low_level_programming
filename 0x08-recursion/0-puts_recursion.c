08267b29f6fdb15943c4e5f0ae143161bea4955c
nelsonombuya <nelson.ombuya@zohomail.com>
2023-04-05 13:44:04 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * _puts_recursion
- * Description: prints a string, followed by
- * a new line
+ * _puts_recursion - prints a string,
+ * followed by a new line
  * @s: character from string
  * Return: Always 0.
  */
