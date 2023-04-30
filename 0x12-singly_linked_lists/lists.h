3bb59c657080084c17b041f48aecedee2bdd71dd
nelsonombuya <nelson.ombuya@zohomail.com>
2023-04-30 11:06:31 -0700

    Update: Tired of Trying

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -16,6 +16,42 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+/* Question 0 Prototype*/
+/* Returns the properties of the linked list */
 size_t print_list(const list_t *h);
+
+/* Question 1 Prototype*/
+/* Returns the number of elements in the linked list */
+size_t list_len(const list_t *h);
+
+/* Question 2 Prototype */
+/* Adds a new node at the beginning of the linked list */
+list_t *add_node(list_t **head, const char *str);
+
+/* Question 3 Prototype */
+/* Adds a new node at the end of the linked list */
+list_t *add_node_end(list_t **head, const char *str);
+
+/* Question 4 Prototype */
+/* Frees a list_t list */
+void free_list(list_t *head);
+
+/* Helper Functions */
+/**
+ * _str_len - Counts the number of characters in a string
+ * @str: The character array
+ * Return: The number of characters in the string
+ */
+size_t _str_len(const char *str)
+{
+	size_t len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
 
 #endif
