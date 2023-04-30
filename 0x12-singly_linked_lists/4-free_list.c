fcc0286498f5e6489b913947d1ce71d0dbe0702b
nelsonombuya <developer@incognitouser.anonaddy.me>
2023-04-30 19:55:46 +0300

    Update: 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees the memory of the elements of a linked list
+ * @h: Pointer to the HEAD of the linked list
+ * Return: Void
+ */
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next_node;
+
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current->str);
+		free(current);
+		current = next_node;
+	}
+}
