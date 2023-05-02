3e92839e1985d3501a174de4ca87753bcd953464
nelsonombuya <nelson.ombuya@zohomail.com>
2023-05-02 16:08:18 -0700

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -46,4 +46,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 /* Question 8 */
 int sum_listint(listint_t *head);
 
+/* Question 9 */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 #endif /* LISTS_H */
