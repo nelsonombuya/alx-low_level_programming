b2dd366a065028b305ac0a18b6dcb77ccf195c87
nelsonombuya <developer@incognitouser.anonaddy.me>
2023-05-04 21:31:06 +0300

    Fix: 0-binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,16 +7,27 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int ui = 0;
+	int len = 0;
+	int base_two_num;
+
 	if (!b)
 		return (0);
 
-	unsigned int ui = 0;
+	while (b[len] != '\0')
+		len++;
 
-	for (; *b != '\0'; ++b)
+	for (len--, base_two_num = 1; len >= 0; len--, base_two_num *= 2)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-		ui = (ui << 1) | (*b - '0');
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two_num;
+		}
 	}
 
 	return (ui);
