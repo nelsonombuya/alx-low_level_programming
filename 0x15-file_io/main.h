a08131161af6c833b005216712409674488a1e56
nelsonombuya <developer@incognitouser.anonaddy.me>
2023-05-08 23:20:03 +0300

    New: 0x15-file_io

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <sys/types.h>
+
+/* _putchar */
+int _putchar(char c);
+
+/* Question 0 */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* MAIN_H */
