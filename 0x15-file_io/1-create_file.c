df2eadc9144d26f8b3923c53bea9a486ffc8e436
nelsonombuya <developer@incognitouser.anonaddy.me>
2023-05-09 21:52:14 +0300

    Fix: 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,10 +14,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content != NULL)
-	{
-		for (length = 0; text_content[length]; length++)
-			continue;
-	}
+		for (length = 0; text_content[length];)
+			length++;
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	fileWrite = write(file, text_content, length);
