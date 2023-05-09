13479ea32c4ecef12d784f749600d0e14be16b12
nelsonombuya <developer@incognitouser.anonaddy.me>
2023-05-09 21:55:17 +0300

    Fix: 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,19 +3,20 @@
 /**
  * create_file - Creates a file
  * @filename: A pointer to the name of the file to be created
- * @text_content: The content to be written to the file during creation
- * Return: -1 upon failure; 1 otherwise.
+ * @text_content: A pointer to the content to be written into the
+ * file during creation
+ * Return: -1 upon failure, 1 otherwise.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, fileWrite, length;
+	int file, fileWrite, length = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
-		for (length = 0; text_content[length];)
-			length++;
+		for (length = 0; text_content[length]; length++)
+			continue;
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	fileWrite = write(file, text_content, length);
